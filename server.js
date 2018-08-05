43057acda2221fd3e2e1a904fa430b6cc8d2317d
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-08-05 00:30:21 -0700

    added server listen and html page links

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const PORT = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+app.get("/api/survey", function(req, res) {
+  return res.json(characters);
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
