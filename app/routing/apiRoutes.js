8f542acef976ace4281d4bffb7d708986a1cd629
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-08-08 13:15:34 -0700

    updated apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,26 @@
 const path = require("path");
+require('../data/friends.js')(app)
 module.exports = function(app){
-  app.get('/api/friends', function(req, res){
+  app.get("/api/friends", function(req, res){
   
-      res.send(html, "../data/friends.js")
+      return res.json(friends)
   });
 
+
   app.post("/api/friends", function (req, res) {
+    let searchFriend = req.body;
+    friends.push(searchFriend);
+    console.log(friends)
     res.sendFile(path.join(__dirname, "../data/friends.js"));
+  });
+  $.get("/api/friends/", function (data) {
+    data.forEach(function (friends, index) {
+      if (index < 5) {
+        $("#friends").append($(`<h2>
+            <span class="label label-primary">${index + 1}</span>
+            <span id="name">${friends.name}</span>
+          </h2>`))
+      } 
+    })
   });
 }
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const fs = require('fs')
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 require('./app/routing/htmlRoutes.js')(app);
+require('./app/data/friends')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.get("/api/friends", function(req, res){
+  
+  return res.json(friends)
+});
+
+
+app.post("/api/friends", function (req, res) {
+let searchFriend = req.body;
+friends.push(searchFriend);
+console.log(friends)
+res.sendFile(path.join(__dirname, "../data/friends.js"));
+});
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
