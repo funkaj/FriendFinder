040240e548d40fbca95f5fc2ed189425d1557599
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-08-09 15:11:11 -0700

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,16 @@
-const path = require("path");
-require('../data/friends.js')(app)
-module.exports = function(app){
-  app.get("/api/friends", function(req, res){
-  
-      return res.json(friends)
-  });
+const path = require("path")
+friends = require('../data/friends.js')
 
+module.exports = function (app) {
+
+  app.get("/api/friends", function (req, res) {
+    return res.json(friends)
+  });
 
   app.post("/api/friends", function (req, res) {
-    let searchFriend = req.body;
-    friends.push(searchFriend);
+    let searchFriend = req.body
+    friends.push(searchFriend)
     console.log(friends)
-    res.sendFile(path.join(__dirname, "../data/friends.js"));
-  });
-  $.get("/api/friends/", function (data) {
-    data.forEach(function (friends, index) {
-      if (index < 5) {
-        $("#friends").append($(`<h2>
-            <span class="label label-primary">${index + 1}</span>
-            <span id="name">${friends.name}</span>
-          </h2>`))
-      } 
-    })
+    res.sendFile(path.join(__dirname, "../data/friends.js"))
   });
 }
--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 const fs = require('fs')
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const PORT = 8080;
-require('./app/routing/htmlRoutes.js')(app);
-require('./app/data/friends')
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const PORT = 8080
+require('./app/routing/htmlRoutes.js')(app)
+require('./app/routing/apiRoutes.js')(app)
 
-app.get("/api/friends", function(req, res){
-  
-  return res.json(friends)
-});
-
-
-app.post("/api/friends", function (req, res) {
-let searchFriend = req.body;
-friends.push(searchFriend);
-console.log(friends)
-res.sendFile(path.join(__dirname, "../data/friends.js"));
-});
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
 
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
+  console.log("App listening on PORT " + PORT)
+})
