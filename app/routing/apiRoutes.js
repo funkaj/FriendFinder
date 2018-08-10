c7216faf68f4a65a54ae930ad575380d772c0995
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-08-10 16:33:07 -0700

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,39 @@
 const fs = require('fs')
 const path = require("path")
 require('../data/friends.js')
-const bodyParser = require("body-parser")
+
 
 module.exports = function (app) {
   app.get("/api/friends", function (req, res) {
     return res.json(friends)
-  });
-  
+  })
+
   app.post("/api/friends", function (req, res) {
-    let newFriend = req.body
-    console.log('userInput = ' + newFriend);
+    newFriend = req.body
+    let answers = newFriend.scores
+    let matchName = ''
+    let matchImage = ''
+    let totalDifference = 100
+
+    for (let i in friends) {
+      let diff = parseInt(0);
+      for (let j in answers) {
+        diff += Math.abs(parseInt(friends[i].scores) - parseInt(answers[j]))
+      
+        if (diff <= totalDifference) {
+          totalDifference = diff
+          matchName = friends[i].name
+          matchImage = friends[i].photo
+        }
+      }
+    }
+    
     friends.push(newFriend)
     
-  });
+    res.json({
+      status: 'OK',
+      matchName: matchName,
+      matchImage: matchImage
+    })
+  })
 }
--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const fs = require('fs')
-const path = require("path")
 const express = require("express")
 const bodyParser = require("body-parser")
+
 const app = express()
 const PORT = process.env.PORT || 8080
 
-//public directory
-app.use(express.static(path.join(__dirname, './app/public')));
-//routes
-require('./app/routing/htmlRoutes.js')(app)
-require('./app/routing/apiRoutes.js')(app)
 //parsing data
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+//routes
+require('./app/routing/apiRoutes.js')(app)
+require('./app/routing/htmlRoutes.js')(app)
 //listener
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT)
